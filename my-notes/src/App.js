de2d2c63527baaeaa5868d5e62ea9933993ffd00
Philippe Santos Silveira <philippe.silveira@cwi.com.br>
2018-09-28 18:25:30 -0300

    'fix(my-notes):_adding_redux'

--- my-notes/src/App.js
+++ my-notes/src/App.js
@@ -1,15 +1,25 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux'
+
+import appActions from './reducers/appActions'
+
 import './App.css';
 
-// scenes
-import Home from './scenes/home'
-
 class App extends Component {
+  changeText() {
+    this.props.dispatch(appActions.changeText('Novo texto'))
+  }
+
   render() {
     return (
-      <Home />
-    );
+      <div>
+        <h1>{this.props.text}</h1>
+        <button onClick={() => this.changeText()}>Mudar texto</button>
+      </div>
+    )
   }
 }
 
-export default App;
+export default connect(store => {
+  return { text: store.text }
+})(App)
---
+++ my-notes/src/reducers/appActions.js
@@ -0,0 +1,8 @@
+export default {
+    changeText(text){
+        return {
+            type: 'CHANGE_TEXT',
+            payload: text
+        }
+    }
+}
